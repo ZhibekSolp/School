8af730671925b406b9e74578c6fc5ba13b95f9d4
dyyura <anarayaq@gmail.com>
2021-09-11 19:37:16 +0600

    ss

--- pupils/models.py
+++ pupils/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Pupils(models.Model):
+    name = models.CharField(verbose_name='ФИО', max_length=255)
+    clas = models.CharField(verbose_name='Касс', max_length=255)
+    number = models.IntegerField(verbose_name='Номер родителей')
+    location = models.CharField(verbose_name='Место проживания', max_lengggth=255)
+    image = models.ImageField(upload_to='/image/', verbose_name='Фото')
